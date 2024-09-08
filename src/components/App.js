e5527c721686fab9b0de0004d7c1c6a21171ba5b
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-09-08 19:32:12 +0000

    new code

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const App = () => {
+
+const relatives=[
+  {key:'uncle',name:"Rinku"},
+  {key:'Aunt',name:'Priya'},
+  {key:'Masi',name:'Sippi'},
+  {key:'sister',name:'Parul'},
+  {key:'mama',name:'Deepak'}
+]
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+              <ol key="relativesList">
+               {relatives.map(relative=>(
+                <li key={relative.key}>{relative.name}</li>
+               ))}
+              </ol>
     </div>
   )
 }
