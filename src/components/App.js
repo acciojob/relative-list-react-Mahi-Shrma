ae20b469f15a3a58694cd659603598ec6a7c03f8
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-09-08 19:47:11 +0000

    again changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,16 @@ import React from 'react'
 const App = () => {
 
 const relatives=[
-  {key:'relativeListItem1',name:"Rinku"},
-  {key:'relativeListItem2',name:'Priya'},
-  {key:'relativeListItem3',name:'Sippi'},
-  {key:'relativeListItem4',name:'Parul'},
-  {key:'relativeListItem5',name:'Deepak'}
-]
+  {key:'#relativeListItem1',name:"Rinku"},
+  {key:'#relativeListItem2',name:'Priya'},
+  {key:'#relativeListItem3',name:'Sippi'},
+  {key:'#relativeListItem4',name:'Parul'},
+  {key:'#relativeListItem5',name:'Deepak'}
+];
 
   return (
     <div id="main">
-              <ol key="relativeList">
+              <ol key='#relativeList'>
                {relatives.map(relative=>(
                 <li key={relative.key}>{relative.name}</li>
                ))}
